d75bc30fa6c8ec80a8638a4ffa6527e6d70e9b34
pfugate <pfugate@princeton.edu>
2016-04-27 18:49:08 -0400

    fixed edge case with just one value and no value

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ bool quit = false;
 
 int main() {
     cout << "You are about to enter the... Mean Median and Mode ZONE!!" << endl;
-    while(!quit) {
+    while (!quit) {
         cout << "Please enter a number (anything but a number starts computation): ";
         cin >> n;
 
@@ -24,48 +24,52 @@ int main() {
             cout << endl;
         }
     }
+    if (inputVector.size() > 0) {
+        sort(inputVector.begin(), inputVector.end());
+        cout << "Your awesome sorted vector:";
+        for (vector<int>::iterator it = inputVector.begin(); it != inputVector.end(); it++) {
+            cout << ' ' << *it;
+            average = average + *it;
+            auto nxt = next(it);
+            if (*it == *nxt) {
+                currentModeCount++;
+                currentModeTarget = *it;
+                if (currentModeCount == modeCount) {
+                    multiModal = true;
+                }
+                else if (currentModeCount > modeCount) {
+                    modeCount = currentModeCount;
+                    mode = currentModeTarget;
+                    multiModal = false;
+                }
+            }
+            else {
+                currentModeCount = 1;
+                currentModeTarget = *it;
+            }
 
-    sort(inputVector.begin(), inputVector.end());
-    cout << "Your awesome sorted vector:";
-    for(vector<int>::iterator it = inputVector.begin(); it != inputVector.end(); it++) {
-        cout << ' ' << *it;
-        average = average + *it;
-        auto nxt = next(it);
-        if(*it == *nxt) {
-            currentModeCount++;
-            currentModeTarget = *it;
-            if(currentModeCount == modeCount) {
-                multiModal = true;
-            }
-            else if(currentModeCount > modeCount) {
-                modeCount = currentModeCount;
-                mode = currentModeTarget;
-                multiModal = false;
-            }
+        }
+        if (inputVector.size() < 2) {
+            mode = currentModeTarget;
+            multiModal = false;
+        }
+        if (inputVector.size() % 2 == 0) {
+            median = (inputVector.at(inputVector.size() / 2) + inputVector.at((inputVector.size() / 2) - 1)) / 2;
         }
         else {
-            currentModeCount = 1;
-            currentModeTarget = *it;
+            median = inputVector.at(inputVector.size() / 2);
         }
-
-    }
-    if(inputVector.size() < 2) {
-        mode = currentModeTarget;
-        multiModal = false;
-    }
-    if (inputVector.size() % 2 == 0) {
-        median = (inputVector.at(inputVector.size()/2) + inputVector.at((inputVector.size()/2)-1))/2;
-    }
-    else {
-        median = inputVector.at(inputVector.size()/2);
-    }
-    cout << endl << endl << "MEAN: " << average/inputVector.size() << endl;
-    cout << "MEDIAN: " << median << endl;
-    if(multiModal) {
-        cout << "MODE: Multi-Modal";
+        cout << endl << endl << "MEAN: " << average / inputVector.size() << endl;
+        cout << "MEDIAN: " << median << endl;
+        if (multiModal) {
+            cout << "MODE: Multi-Modal";
+        }
+        else {
+            cout << "MODE: " << mode << endl;
+        }
     }
     else {
-        cout << "MODE: " << mode << endl;
+        cout << "Umm I need numbers to do my thing..." << endl;
     }
     return 0;
 }
